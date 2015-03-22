880188639f3e42041d1e242e0dfedf92e4e46ac6
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-03-22 21:27:58 +0100

    Unittest calling all logging functions
    Still miss the headers

--- UnitTest/UnitTest1.cs
+++ UnitTest/UnitTest1.cs
@@ -7,10 +7,39 @@ namespace UnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void TestMethod1()
+        public void TestMethod_Init()
         {
             var MyLogger = new TestResultHtmlLogger.TestResultHtmlLogger();
             MyLogger.Init(@"c:\temp\unittestlogger.html");
         }
+
+        [TestMethod]
+        public void TestMethod_AllLogType()
+        {
+            var MyLogger = new TestResultHtmlLogger.TestResultHtmlLogger();
+            MyLogger.Init(@"c:\temp\unittestlogger.html");
+
+            MyLogger.LogError("LogError");
+            MyLogger.Error("Error");
+            MyLogger.LogWarning("LogWarning");
+            MyLogger.Warning("Warning");
+            MyLogger.LogInfo("LogInfo");
+            MyLogger.Info("Info");
+            MyLogger.LogDebug("LogDebug");
+            MyLogger.Debug("Debug");
+
+        // normal logging functions - models and adapters
+            MyLogger.LogTrace("LogTrace");
+            MyLogger.Trace("Trace");
+            MyLogger.LogInternal("LogInternal");
+            MyLogger.Internal("Internal");
+
+        // used solely by Assert functions
+            MyLogger.LogPass("testStepName LogPass", "LogPass");
+            MyLogger.Pass("testStepName Pass", "Pass");
+            MyLogger.LogFail("testStepName LogFail", "LogFail");
+            MyLogger.Fail("testStepName Fail", "Fail");
+
+            MyLogger.LogKeyValue("SomeKey", "SomeValue", "LogKeyValue");        }
     }
 }
