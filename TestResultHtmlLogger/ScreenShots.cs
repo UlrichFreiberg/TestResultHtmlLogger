0d92d895294d6677c7dac5f65b0d92456c2fb911
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-03-25 22:49:47 +0100

    Template HTML code for IMG

--- TestResultHtmlLogger/ScreenShots.cs
+++ TestResultHtmlLogger/ScreenShots.cs
@@ -26,6 +26,15 @@ namespace TestResultHtmlLogger
         int IScreenshots.LogImage(LogLevel logLevel, string imageFile, string message)
         {
             throw new NotImplementedException();
+
+          //            messageIdString = GetNextMessageId();
+
+//      <div onclick="sa('m413')" id="m413" class="line info image">
+//          <div class="el time">17:11:56</div>
+//          <div class="el level">info</div>
+//          <div class="el msg">Billede: Waited for this testobject to appear For [0] seconds. TypeName[Step] micclass[Page]</div>
+//          <p><img  onclick="showImage(this)" class="embeddedimage" alt="Waited for this testobject to appear For [0] seconds. TypeName[Step] micclass[Page]" src="data:image/png;base64, iVBORw0KGgoAAAANSUhEUgAAB4AAAAPpCAAAAADRfW+bAAAABGdBTUEAA
+//      </div>
         }
     }
 }
