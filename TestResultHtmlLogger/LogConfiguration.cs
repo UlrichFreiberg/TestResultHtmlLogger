7ea1c896054784f2d93404f665f5bf5ae23376ff
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-03-27 23:36:53 +0100

    First shot at a LogFile app.config
    Missing a Cast for LogLevel

--- TestResultHtmlLogger/LogConfiguration.cs
+++ TestResultHtmlLogger/LogConfiguration.cs
@@ -1,13 +1,34 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.Globalization;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace TestResultHtmlLogger
 {
+    static class Settings
+    {
+        static UriBuilder uri = new UriBuilder(Assembly.GetExecutingAssembly().CodeBase);
+        static System.Configuration.Configuration myDllConfig = ConfigurationManager.OpenExeConfiguration(uri.Path);
+        static AppSettingsSection AppSettings = (AppSettingsSection)myDllConfig.GetSection("appSettings");
+        static NumberFormatInfo nfi = new NumberFormatInfo()
+        {
+            NumberGroupSeparator = "",
+            CurrencyDecimalSeparator = "."
+        };
+
+        public static T Setting<T>(string name)
+        {
+            return (T)Convert.ChangeType(AppSettings.Settings[name].Value, typeof(T), nfi);
+        }
+    }
+
     public class LogConfiguration
     {
+
         public Boolean OverwriteLogFile { get; set; }
         public Boolean LogToFile { get; set; }
         public String LogTitle { get; set; }
@@ -22,13 +43,13 @@ namespace TestResultHtmlLogger
         /// </summary>
         public LogConfiguration()
         {
-            OverwriteLogFile = true;
-            LogToFile = false;
-            LogTitle = "A Working Title";
-            LogFileName = @"C:\temp\TestResultHtmlLogger.html";
-            AlertLongInterval = 10000;
-            LogLevel = LogLevel.Error;
-            PathToLogoImageFile = String.Empty;
+            OverwriteLogFile = Settings.Setting<Boolean>("OverwriteLogFile");
+            LogToFile = Settings.Setting<Boolean>("LogToFile");
+            LogTitle = Settings.Setting<String>("LogTitle");
+            LogFileName = Settings.Setting<String>("LogFileName"); 
+            AlertLongInterval = Settings.Setting<int>("AlertLongInterval");
+            LogLevel = LogLevel.Info; //TODO: Settings.Setting<LogLevel>("LogLevel");
+            PathToLogoImageFile = Settings.Setting<String>("PathToLogoImageFile");
         }
     }
 }
