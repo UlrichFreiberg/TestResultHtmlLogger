757049cc2cf00be93b75e91a21696a8ac2d21850
kaweera <kaweera@gmail.com>
2015-03-28 17:22:26 +0100

    Added rudimentary screenshot logging functionality. Added stylecop settings, which probably needs a bit of tweaking.

--- TestResultHtmlLogger/Interfaces/IScreenShots.cs
+++ TestResultHtmlLogger/Interfaces/IScreenShots.cs
@@ -1,15 +1,62 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="IScreenShots.cs" company="Foobar">
+//   2015
+// </copyright>
+// <summary>
+//   The Screenshots interface.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
 
 namespace TestResultHtmlLogger
 {
+    /// <summary>
+    /// The Screenshots interface.
+    /// </summary>
     public interface IScreenshots
     {
-	    int LogAllWindows(LogLevel logLevel, string message);
-	    int LogScreenshot(LogLevel logLevel, string message);
-	    int LogImage(LogLevel logLevel, string imageFile, string message);
+        /// <summary>
+        /// The log all windows.
+        /// </summary>
+        /// <param name="logLevel">
+        /// The log level.
+        /// </param>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        int LogAllWindows(LogLevel logLevel, string message);
+
+        /// <summary>
+        /// The log screenshot.
+        /// </summary>
+        /// <param name="logLevel">
+        /// The log level.
+        /// </param>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        int LogScreenshot(LogLevel logLevel, string message);
+
+        /// <summary>
+        /// The log image.
+        /// </summary>
+        /// <param name="logLevel">
+        /// The log level.
+        /// </param>
+        /// <param name="imageFile">
+        /// The image file.
+        /// </param>
+        /// <param name="message">
+        /// The message.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
+        int LogImage(LogLevel logLevel, string imageFile, string message);
     }
 }
