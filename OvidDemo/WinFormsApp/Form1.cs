6d9e559ed903fd7b7d1e9c9b8243ea0941a9c19d
Ulrich Freiberg <Ulrich@mirsoftware.com>
2015-04-09 19:45:12 +0200

    Fixed OvidDemo - new namespaces and stuff

--- OvidDemo/WinFormsApp/Form1.cs
+++ OvidDemo/WinFormsApp/Form1.cs
@@ -3,16 +3,15 @@
 //   2015
 // </copyright>
 // --------------------------------------------------------------------------------------------------------------------
+
 namespace WinFormsApp
 {
     using System;
     using System.Diagnostics;
-    using System.Drawing.Text;
     using System.Windows.Forms;
 
-    using Stf.Utilities.StfAssert;
-    using Stf.Utilities.TestResultHtmlLogger;
-    using Stf.Utilities.TestResultHtmlLogger.Interfaces;
+    using Stf.Utilities;
+    using Stf.Utilities.Interfaces;
 
     /// <summary>
     /// The form 1.
@@ -99,31 +98,85 @@ namespace WinFormsApp
             Application.Exit();
         }
 
+        /// <summary>
+        /// The text box 1_ text changed.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
 
         }
 
+        /// <summary>
+        /// The btn log info_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void btnLogInfo_Click(object sender, EventArgs e)
         {
             this.Mylogger.LogInfo("Info: " + this.TxtMessage.Text);
         }
 
+        /// <summary>
+        /// The btn log trace_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void btnLogTrace_Click(object sender, EventArgs e)
         {
             this.Mylogger.LogTrace("Info: " + this.TxtMessage.Text);
         }
 
+        /// <summary>
+        /// The btn log pass_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void btnLogPass_Click(object sender, EventArgs e)
         {
             this.Mylogger.LogPass("OvidDemo", this.TxtMessage.Text);
         }
 
+        /// <summary>
+        /// The btn log fail_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void btnLogFail_Click(object sender, EventArgs e)
         {
             this.Mylogger.LogFail("OvidDemo", this.TxtMessage.Text);
         }
 
+        /// <summary>
+        /// The call stack demo.
+        /// </summary>
+        /// <param name="argument">
+        /// The argument.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
         private int callStackDemo(string argument)
         {
             var retVal = 40;
@@ -137,6 +190,15 @@ namespace WinFormsApp
             return retVal;
         }
 
+        /// <summary>
+        /// The call stack demo l 1.
+        /// </summary>
+        /// <param name="argument">
+        /// The argument.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
         private int callStackDemoL1(string argument)
         {
             var retVal = 41;
@@ -149,6 +211,15 @@ namespace WinFormsApp
             return retVal;
         }
 
+        /// <summary>
+        /// The call stack demo l 2.
+        /// </summary>
+        /// <param name="argument">
+        /// The argument.
+        /// </param>
+        /// <returns>
+        /// The <see cref="int"/>.
+        /// </returns>
         private int callStackDemoL2(string argument)
         {
             var retVal = 42;
@@ -159,34 +230,102 @@ namespace WinFormsApp
             return retVal;
         }
 
+        /// <summary>
+        /// The btn assert string equals_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void BtnAssertStringEquals_Click(object sender, EventArgs e)
         {
             this.MyAssert.StringEquals("DemoApp", this.TxtStringAssertArg1.Text, this.TxtStringAssertArg2.Text);
         }
 
+        /// <summary>
+        /// The btn assert string matches_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void BtnAssertStringMatches_Click(object sender, EventArgs e)
         {
             this.MyAssert.StringMatches("DemoApp", this.TxtStringAssertArg1.Text, this.TxtStringAssertArg2.Text);
         }
 
+        /// <summary>
+        /// The btn assert string contains_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void BtnAssertStringContains_Click(object sender, EventArgs e)
         {
             this.MyAssert.StringContains("DemoApp", this.TxtStringAssertArg1.Text, this.TxtStringAssertArg2.Text);
         }
 
+        /// <summary>
+        /// The btn assert equals_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void BtnAssertEquals_Click(object sender, EventArgs e)
         {
             this.MyAssert.AssertEquals("DemoApp", this.TxtAssertArg1.Text, this.TxtAssertArg2.Text);
         }
 
+        /// <summary>
+        /// The btn assert has value_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void BtnAssertHasValue_Click(object sender, EventArgs e)
         {
             this.MyAssert.AssertHasValue("DemoApp", this.TxtAssertArg1.Text);
         }
 
+        /// <summary>
+        /// The btn assert null_ click.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
         private void BtnAssertNull_Click(object sender, EventArgs e)
         {
             this.MyAssert.AssertNull("DemoApp", this.TxtAssertArg1.Text);
+        }
+
+        /// <summary>
+        /// The form 1_ load.
+        /// </summary>
+        /// <param name="sender">
+        /// The sender.
+        /// </param>
+        /// <param name="e">
+        /// The e.
+        /// </param>
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
